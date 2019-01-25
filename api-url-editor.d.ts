4593f4611afb299db8266b1ac90df3b55f6b89b9
Pawel Psztyc <jarrodek@gmail.com>
2019-01-24 17:29:10 -0800

    Docs: Updating documentation

--- api-url-editor.d.ts
+++ api-url-editor.d.ts
@@ -8,6 +8,10 @@
  *   api-url-editor.html
  */
 
+
+// tslint:disable:variable-name Describing an API that's defined elsewhere.
+// tslint:disable:no-any describes the API as best we are able today
+
 /// <reference path="../polymer/types/polymer-element.d.ts" />
 /// <reference path="../paper-input/paper-input-container.d.ts" />
 /// <reference path="../paper-input/paper-input-behavior.d.ts" />
